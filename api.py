e86d904b2883411c28731740e240c0aae0dbc019
kobakakzu0429 <kobakazu10@gmail.com>
2018-04-29 08:51:01 +0900

    refactor @api.route

--- api.py
+++ api.py
@@ -29,91 +29,115 @@ class Data(peewee.Model):
 
 api = Flask(__name__)
 
-@api.route('/api/v1/publishing_task/', methods=['GET', 'POST'])
-def get_radios():
+@api.route('/api/v1/publishing_task/', methods=['GET'])
+def get_publishing_tasks():
     try:
       db.connect()
     except Data.DoesNotExist:
         abort(404)
 
-    if request.method == 'POST':
-        Data.create(title         = request.form['title'],
-                    published_at  = request.form['published_at'],
-                    rec           = request.form['rec'],
-                    edit          = request.form['edit'],
-                    censorship    = request.form['censorship'],
-                    thumbnail     = request.form['thumbnail'],
-                    reserve       = request.form['reserve'],
-                    release       = request.form['release'],
-                    comic         = request.form['comic'],
-                    tweet         = request.form['tweet'],
-                    folder_id     = request.form['folder_id'],
-                    rec_url       = request.form['rec_url'],
-                    thumbnail_url = request.form['thumbnail_url'],
-                    comic_url     = request.form['comic_url'])
+    cursor = db.execute_sql('select * from data;')
 
-        return make_response(jsonify({'result': 'Uploaded'}), 200)
+    datas = []
+    for row in cursor.fetchall():
+        x = dict(zip([d[0] for d in cursor.description], row))
+        datas.append(dict(x))
 
-    elif request.method == 'GET':
-        cursor = db.execute_sql('select * from data;')
-
-        datas = []
-        for row in cursor.fetchall():
-            x = dict(zip([d[0] for d in cursor.description], row))
-            datas.append(dict(x))
-
-        return convert_for_response(datas)
-
+    return convert_for_response(datas)
     db.close()
 
 
-@api.route('/api/v1/publishing_task/<string:id>', methods=['GET', 'POST', 'DELETE'])
-def crud_radio(id):
+@api.route('/api/v1/publishing_task/', methods=['POST'])
+def create_publishing_task():
     try:
-        db.connect()
-        radio = Data.get(Data.id == id)
+      db.connect()
     except Data.DoesNotExist:
         abort(404)
 
-    if request.method == 'GET':
-        cursor = db.execute_sql('select * from data where id =' + id)
+    Data.create(title         = request.form['title'],
+                published_at  = request.form['published_at'],
+                rec           = request.form['rec'],
+                edit          = request.form['edit'],
+                censorship    = request.form['censorship'],
+                thumbnail     = request.form['thumbnail'],
+                reserve       = request.form['reserve'],
+                release       = request.form['release'],
+                comic         = request.form['comic'],
+                tweet         = request.form['tweet'],
+                folder_id     = request.form['folder_id'],
+                rec_url       = request.form['rec_url'],
+                thumbnail_url = request.form['thumbnail_url'],
+                comic_url     = request.form['comic_url'])
 
-        datas = []
-        for row in cursor.fetchall():
-            x = dict(zip([d[0] for d in cursor.description], row))
-            datas.append(dict(x))
-
-        return convert_for_response(datas)
-
-    elif request.method == 'POST':
-        radio.title         = request.form['title']
-        radio.published_at  = request.form['published_at']
-        radio.rec           = request.form['rec']
-        radio.edit          = request.form['edit']
-        radio.censorship    = request.form['censorship']
-        radio.thumbnail     = request.form['thumbnail']
-        radio.reserve       = request.form['reserve']
-        radio.release       = request.form['release']
-        radio.comic         = request.form['comic']
-        radio.tweet         = request.form['tweet']
-        radio.folder_id     = request.form['folder_id']
-        radio.rec_url       = request.form['rec_url']
-        radio.thumbnail_url = request.form['thumbnail_url']
-        radio.comic_url     = request.form['comic_url']
-
-        radio.save()
-
-        return make_response(jsonify({'result': 'Updated'}), 200)
-
-    elif request.method == 'DELETE':
-        del_data = Data.get(Data.id == id)
-        del_data.delete_instance()
-
-        db.commit()
-
-        return make_response(jsonify({'result': 'Deleted'}), 200)
-
+    return make_response(jsonify({'result': 'Uploaded'}), 200)
     db.close()
+
+
+@api.route('/api/v1/publishing_task/<string:id>/', methods=['GET'])
+def get_publishing_task(id):
+    try:
+        db.connect()
+        publishing_task = Data.get(Data.id == id)
+    except Data.DoesNotExist:
+        abort(404)
+
+    cursor = db.execute_sql('select * from data where id =' + id)
+
+    print(publishing_task)
+    print(cursor)
+
+    datas = []
+    for row in cursor.fetchall():
+        x = dict(zip([d[0] for d in cursor.description], row))
+        datas.append(dict(x))
+
+    return convert_for_response(datas)
+    db.close()
+
+
+@api.route('/api/v1/publishing_task/<string:id>/', methods=['PATCH'])
+def update_publishing_task(id):
+    try:
+        db.connect()
+        publishing_task = Data.get(Data.id == id)
+    except Data.DoesNotExist:
+        abort(404)
+
+    publishing_task.title         = request.form['title']
+    publishing_task.published_at  = request.form['published_at']
+    publishing_task.rec           = request.form['rec']
+    publishing_task.edit          = request.form['edit']
+    publishing_task.censorship    = request.form['censorship']
+    publishing_task.thumbnail     = request.form['thumbnail']
+    publishing_task.reserve       = request.form['reserve']
+    publishing_task.release       = request.form['release']
+    publishing_task.comic         = request.form['comic']
+    publishing_task.tweet         = request.form['tweet']
+    publishing_task.folder_id     = request.form['folder_id']
+    publishing_task.rec_url       = request.form['rec_url']
+    publishing_task.thumbnail_url = request.form['thumbnail_url']
+    publishing_task.comic_url     = request.form['comic_url']
+
+    publishing_task.save()
+
+    return make_response(jsonify({'result': 'Updated'}), 200)
+    db.close()
+
+@api.route('/api/v1/publishing_task/<string:id>/', methods=['DELETE'])
+def delete_publishing_task(id):
+    try:
+        db.connect()
+        delete_publishing_task = Data.get(Data.id == id)
+    except Data.DoesNotExist:
+        abort(404)
+
+    delete_publishing_task.delete_instance()
+
+    db.commit()
+
+    return make_response(jsonify({'result': 'Deleted'}), 200)
+    db.close()
+
 
 @api.errorhandler(404)
 def not_found(error):
